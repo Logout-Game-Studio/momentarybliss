98d30519cf4c304eb598ec76f19878051a853c93
Logout <matheus.bertos2@gmail.com>
2024-09-15 14:42:53 -0300

    Update raindrop.gd

--- nodes/obstacle/raindrop.gd
+++ nodes/obstacle/raindrop.gd
@@ -1,6 +1,8 @@
-extends RigidBody2D
+class_name RainDrop extends RigidBody2D
 
 @export var damage = 1
+
+signal attacked
 
 func _on_attack_area_area_entered(area: Area2D) -> void:
 	if area is HitBoxArea:
