68e5284a710ede52afb01d493113f6af17d6de3f
Logout <matheus.bertos2@gmail.com>
2024-09-19 12:06:02 -0300

    added cloud particles to the game

--- nodes/obstacle/moving_obstacle.gd
+++ nodes/obstacle/moving_obstacle.gd
@@ -6,6 +6,7 @@ var current_bounces: int = 0
 var move_speed: float = 5
 @export var max_move_speed: float = 6
 @export var min_move_speed: float = 3
+@export var particle_on_despawn:PackedScene
 
 func _ready() -> void:
 	move_speed = randf_range(min_move_speed,max_move_speed) #randomize speed
@@ -24,4 +25,13 @@ func _on_corner_detection_area_body_entered(body: Node2D) -> void:
 	current_bounces += 1
 	direction *= -1
 	if current_bounces >= max_bounces:
+		if particle_on_despawn:
+			_instantiate_particle()
 		queue_free()
+
+func _instantiate_particle():
+	var particle_instance:GPUParticles2D = particle_on_despawn.instantiate()
+	get_tree().current_scene.add_child(particle_instance)
+	particle_instance.global_position = self.global_position
+	particle_instance.emitting = true
+	particle_instance.finished.connect(func(): particle_instance.queue_free())
